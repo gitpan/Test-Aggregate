95748e90bddd85cab4f8224b9bc6a47c66c4f188
Randy Stauner <rwstauner@cpan.org>
2013-09-01 22:30:03 +0000

    Import of RWSTAUNER/Test-Aggregate-0.370 from CPAN.
    gitpan-cpan-distribution: Test-Aggregate
    gitpan-cpan-version:      0.370
    gitpan-cpan-path:         RWSTAUNER/Test-Aggregate-0.370.tar.gz
    gitpan-cpan-author:       RWSTAUNER
    gitpan-cpan-maturity:     released

--- lib/Test/Aggregate.pm
+++ lib/Test/Aggregate.pm
@@ -25,11 +25,11 @@ Test::Aggregate - Aggregate C<*.t> tests to make them run faster.
 
 =head1 VERSION
 
-Version 0.369
+Version 0.370
 
 =cut
 
-our $VERSION = '0.369';
+our $VERSION = '0.370';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
@@ -466,7 +466,7 @@ $findbin
         if ( $setup ) {
             $code .= "    $setup->('$test');\n";
         }
-        $code .= qq{    run_this_test_program( $package => "$test", $current_test, $total_tests, $verbose );};
+        $code .= qq{    run_this_test_program( $package => '$test', $current_test, $total_tests, $verbose );};
 
         if ( $teardown ) {
             $code .= "    $teardown->('$test');\n";
